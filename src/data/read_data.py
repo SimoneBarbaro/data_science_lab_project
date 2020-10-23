0f37fe77dd2ad87cedafdf85f419162378dd8d4c
kselanne <64537951+kselanne@users.noreply.github.com>
2020-10-23 22:07:09 +0200

    Sample data returning also drug pair names .gitignore: ignore everything in the data folder except for the README.md

--- src/data/read_data.py
+++ src/data/read_data.py
@@ -23,6 +23,10 @@ def get_spider_data():
     data = pd.read_csv(os.path.join(dirname, "../../data/spider_twosides_table.csv"))
     return data
 
+def get_spider_data_with_names():
+    data = pd.read_excel(os.path.join(dirname, "../../data/spider_twosides_table.xlsx"), index_col="alldrugs_TWOSIDES").drop(columns=["mol_id", "scores_here252"])
+    return data
+    
 
 def create_matrix(data):
     """
@@ -31,22 +35,50 @@ def create_matrix(data):
     :return: A new dataframe where each distinct pair of rows is summed together along the columns.
     """
     X = []
-    for d1 in data.iterrows():
-        for d2 in data.iterrows():
-            if d1[0] < d2[0]:
-                X.append(d1[1] + d2[1])
+    for i1, d1 in data.iterrows():
+        for i2, d2 in data.iterrows():
+            if i1 < i2:
+                X.append(d1 + d2)
     return pd.DataFrame(X)
 
 
 def load_sample(frac=1, random_state=1, save=False):
-    if save and os.path.exists(os.path.join(dirname, "../../data/matrix_spider_{}.npy".format(frac))):
-        return np.load()
+    if save and os.path.exists(os.path.join(dirname, f"../../data/matrix_spider_{frac}_{random_state}.npy")):
+        return np.load(os.path.join(dirname, f"../../data/matrix_spider_{frac}_{random_state}.npy"))
 
     data_sample = get_spider_data_sample(frac=frac, random_state=random_state)
     data = create_matrix(data_sample).values
     if save:
-        np.save(os.path.join(dirname, "../../data/matrix_spider_{}.npy".format(frac)), data)
+        np.save(os.path.join(dirname, f"../../data/matrix_spider_{frac}_{random_state}.npy"), data)
     return data
+
+
+def load_sample_with_names(frac=1, random_state=1, save=False):
+    if save and os.path.exists(os.path.join(dirname, f"../../data/matrix_spider_{frac}_{random_state}.pkl")) and os.path.exists(os.path.join(dirname, f"../../data/matrix_spider_names_{frac}_{random_state}.pkl")):
+        data_matrix = pd.read_pickle(os.path.join(dirname, f"../../data/matrix_spider_{frac}_{random_state}.pkl"))
+        matrix_names = pd.read_pickle(os.path.join(dirname, f"../../data/matrix_spider_names_{frac}_{random_state}.pkl"))
+    else:
+        data_sample_name = get_spider_data_with_names().sample(frac=frac, random_state=random_state)
+        data_matrix = create_matrix(data_sample_name)
+        matrix_names = get_drug_names(data_sample_name)
+        if save:
+            data.to_pickle(os.path.join(dirname, f"../../data/matrix_spider_names_{frac}_{random_state}.pkl"), data)
+    return data_matrix, matrix_names
+
+
+def get_drug_names(data):
+    """
+    Create a pandas dataframe with names of drug pairs corresponding to create_matrix(data).
+    :param data: A pandas dataframe of interactions, indexed with drug names.
+    :return: A pandas dataframe of drug name pairs, name1 | name2.
+    """
+    names = data.index
+    X = []
+    for i1, n1 in pd.DataFrame(names).iterrows():
+        for i2, n2 in pd.DataFrame(names).iterrows():
+            if i1 < i2:
+                X.append({'name1': n1[0], 'name2': n2[0]})
+    return pd.DataFrame(X)
 
 
 def get_drug_index():
