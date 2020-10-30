c40c9e61be2d68a7a5891324b0967b466843eab5
nhutchins627 <56673379+nhutchins627@users.noreply.github.com>
2020-10-29 23:20:31 -0700

    compare clusters

--- src/result_analysis.py
+++ src/result_analysis.py
@@ -5,7 +5,13 @@ from sklearn.feature_extraction.text import TfidfTransformer
 
 from data.read_data import get_twosides_meddra, match_meddra
 
-
+def intersect2D(a, b):
+      """
+      Find row intersection between 2D numpy arrays, a and b.
+      Returns another numpy array with shared rows
+      """
+    return np.array([x for x in set(tuple(x) for x in a) & set(tuple(x) for x in b)])
+    
 class ResultAnalyzer:
     """
     Analyzer for our clustering results.
@@ -53,3 +59,37 @@ class ResultAnalyzer:
 
         final_dataframe = pd.concat([scores_dataframe, tfidf_dataframe], axis=1)
         final_dataframe.to_csv(os.path.join(self.analysis_dir, "scores_{}.csv".format(meddra_term)), index=True, header=True)
+    
+    def cluster_intersection(self, results2):
+        size = []
+        assignments = []
+        for i in range(len(self.results_file["cluster"].unique())):
+            size.append(self.results_file[self.results_file["cluster"] == i].count()["cluster"])
+            assignments.append(self.results_file[self.results_file["cluster"] == i][self.results_file.columns[0:2]].values)
+    
+         assignments_ordered = []
+        ranks = np.argsort(size)
+        for j in range(len(size)):
+            assignments_ordered.append(assignments[ranks[j]])
+    
+        size2 = []
+        assignments2 = []
+        for i in range(len(results2["cluster"].unique())):
+            size2.append(results2[results2["cluster"] == i].count()["cluster"])
+            assignments2.append(results2[results2["cluster"] == i][results2.columns[0:2]].values)
+    
+        assignments_ordered2 = []
+        ranks2 = np.argsort(size2)
+        for j in range(len(size2)):
+            assignments_ordered2.append(assignments2[ranks2[j]])
+    
+        mat = np.zeros((len(assignments_ordered), len(assignments_ordered2)))
+    
+        for i in range(len(assignments_ordered)):
+            for j in range(len(assignments_ordered2)):
+                mat[i, j] = len(intersect2D(assignments_ordered[i], assignments_ordered2[j]))
+    
+        v1 = np.array(size)
+        v2 = np.array(size2)
+    
+        return mat/v1[:, None], mat/v2[None, :]
