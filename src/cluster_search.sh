fca99105a00fd0a4e86ee6b013918c638ff675ca
Simone <simone.barbaro2@studio.unibo.it>
2020-11-10 17:56:48 +0100

    less search for aggl

--- src/cluster_search.sh
+++ src/cluster_search.sh
@@ -5,7 +5,7 @@ for clustering in "kmeans" "gmm" "dbscan" "optics" "mean_shift"; do
     bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/${clustering}_norm.txt python search_clustering_main.py --clustering ${clustering} --clustering_search_config ../config/${clustering}_search.json --normalize --save_result_path ../results/${clustering}_norm.csv
 done
 
-bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl1.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_fixed_n.json --save_result_path ../results/aggl1.csv
-bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl2.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_fixed_n.json --normalize --save_result_path ../results/aggl2.csv
-bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl3.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_no_n.json --save_result_path ../results/aggl3.csv
-bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl4.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_no_n.json --normalize --save_result_path ../results/aggl4.csv
+bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl1.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_fixed_n.json --search_coverage 20 --save_result_path ../results/aggl1.csv
+bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl2.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_fixed_n.json --search_coverage 20 --normalize --save_result_path ../results/aggl2.csv
+bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl3.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_no_n.json --search_coverage 20 --save_result_path ../results/aggl3.csv
+bsub -W 12:00 -n 8 -R "rusage[mem=4096]" -oo ../results/aggl4.txt python search_clustering_main.py --clustering aggl --clustering_search_config ../config/agglomerative_search_no_n.json --search_coverage 20 --normalize --save_result_path ../results/aggl4.csv
