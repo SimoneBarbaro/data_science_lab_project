3cd09a546802ff24da8abd738aef5264ae7bccec
Simone <simone.barbaro2@studio.unibo.it>
2020-11-10 17:33:46 +0100

    no dpgmm search

--- src/cluster_search.sh
+++ src/cluster_search.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 module load python/3.6.0 python/3.6.1
-for clustering in "kmeans" "dpgmm" "gmm" "dbscan" "optics" "mean_shift"; do
+for clustering in "kmeans" "gmm" "dbscan" "optics" "mean_shift"; do
     bsub -W 3:59 -n 8 -R "rusage[mem=4096]" -oo ../results/${clustering}.txt python search_clustering_main.py --clustering ${clustering} --clustering_search_config ../config/${clustering}_search.json --save_result_path ../results/${clustering}.csv
     bsub -W 3:59 -n 8 -R "rusage[mem=4096]" -oo ../results/${clustering}_norm.txt python search_clustering_main.py --clustering ${clustering} --clustering_search_config ../config/${clustering}_search.json --normalize --save_result_path ../results/${clustering}_norm.csv
 done
