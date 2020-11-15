daec8a5656471643f82a9c2c19333dbe79e11577
kselanne <64537951+kselanne@users.noreply.github.com>
2020-11-15 03:27:07 +0100

    Grubb's test to replace mean/std approach

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -1,17 +1,21 @@
 import argparse
 import os
+import warnings
 import numpy as np
 import pandas as pd
+from outliers import smirnov_grubbs as grubbs # pip install outlier_utils
 
 def np_mad(arr, axis=None):
     """Median absolute deviation as alternative to np.std"""
     return 1.4826*np.median(np.abs(arr - np.median(arr, axis)), axis)
 
 if __name__ == "__main__":
-
+    
+    warnings.filterwarnings("ignore")  # ignore Grubb's test "RuntimeWarning: invalid value encountered in double_scalars"
+    
     parser = argparse.ArgumentParser()
     # Sample command for copy-pasting and editing:
-    # python ./statistical_analysis.py --run_name kmeans16 --level hlgt --method ranks --location mean --scatter std --distance 2.5 --print_only
+    # python ./statistical_analysis.py --run_name kmeans16 --level hlgt --method ranks --alpha 0.005 --print_only
     
     parser.add_argument('--run_name', type=str, default="test",
                         help="name of the run where to look for the analysis folder with the scores")
@@ -19,12 +23,8 @@ if __name__ == "__main__":
                         help='choose the medDRA level')
     parser.add_argument('--method', type=str, choices=["ranks", "scores"], default="ranks",
                         help='choose the quantity on which to evaluate statistical significance')
-    parser.add_argument('--location', type=str, choices=["mean", "median"], default="mean",
-                        help='choose the location statistic')
-    parser.add_argument('--scatter', type=str, choices=["std", "mad"], default="std",
-                        help='choose the scatter statistic')
-    parser.add_argument('--distance', type=float, default=3.0,
-                        help="how many scatters from the location is considered significant")
+    parser.add_argument('--alpha', type=float, default=0.005,
+                        help="significance level for Grubb's min/max test")
     parser.add_argument('--print_only', action='store_true', default=False,
                         help="add to only print the results without saving")
 
@@ -36,33 +36,19 @@ if __name__ == "__main__":
     
     term = "{}_term".format(args.level)
     
-    location_method = lambda: None
-    if args.location == "mean":
-        location_method = np.mean
-    elif args.location == "median":
-        location_method = np.median
-    
-    scatter_method = lambda: None
-    if args.scatter == "std":
-        scatter_method = np.std
-    elif args.scatter == "mad":
-        scatter_method = np_mad
-    
     significant = pd.DataFrame()
     if args.method == "ranks":
         for term in ranked.iloc[:,1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
             ranks = ranked[ranked.iloc[:,1] == term]["rank"]  # use ranks
-            location = location_method(ranks)
-            scatter = scatter_method(ranks)
-            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["rank"] < location - args.distance*scatter)])
+            sig_values = grubbs.min_test_outliers(list(ranks), alpha=args.alpha)
+            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["rank"].isin(sig_values))])
     elif args.method == "scores":
         for term in ranked.iloc[:,1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
             tfidfs = ranked[ranked.iloc[:,1] == term]["tfidf_score"]  # use tf-idf scores
-            location = location_method(tfidfs)
-            scatter = scatter_method(tfidfs)
-            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["tfidf_score"] > location + args.distance*scatter)])
+            sig_values = grubbs.max_test_outliers(list(tfidfs), alpha=args.alpha)
+            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["tfidf_score"].isin(sig_values))])
     
-    significant = significant.sort_values("tfidf_score", ascending=False)
+    significant = significant.sort_values("rank", ascending=True)
     
     if not args.print_only:
         results_file = os.path.join("../results", args.run_name, "analysis", "significant_{}_{}_{}_{}_{}.csv".format(args.level, args.method, args.location, args.scatter, args.distance))
