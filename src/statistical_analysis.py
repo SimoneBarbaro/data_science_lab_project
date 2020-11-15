8754ba3ef962257ac051e02b196313b5718870ad
kselanne <64537951+kselanne@users.noreply.github.com>
2020-11-15 19:03:55 +0100

    Add Grubb's test statistic and sorting options

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     # Sample command for copy-pasting and editing:
-    # python ./statistical_analysis.py --run_name kmeans16 --level hlgt --method ranks --alpha 0.005 --print_only
+    # python ./statistical_analysis.py --run_name kmeans16 --level hlgt --method ranks --alpha 0.005 --sort_by rank --print_only
     
     parser.add_argument('--run_name', type=str, default="test",
                         help="name of the run where to look for the analysis folder with the scores")
@@ -25,6 +25,8 @@ if __name__ == "__main__":
                         help='choose the quantity on which to evaluate statistical significance')
     parser.add_argument('--alpha', type=float, default=0.005,
                         help="significance level for Grubb's min/max test")
+    parser.add_argument('--sort_by', type=str, choices=["rank", "grubbs", "tfidf_score"], default="rank",
+                        help='choose the quantity on which to sort the results')
     parser.add_argument('--print_only', action='store_true', default=False,
                         help="add to only print the results without saving")
 
@@ -40,15 +42,26 @@ if __name__ == "__main__":
     if args.method == "ranks":
         for term in ranked.iloc[:,1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
             ranks = ranked[ranked.iloc[:,1] == term]["rank"]  # use ranks
+            mean = np.mean(ranks)
+            std_dev = np.std(ranks)
             sig_values = grubbs.min_test_outliers(list(ranks), alpha=args.alpha)
-            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["rank"].isin(sig_values))])
+            for val in sig_values:
+                grubbs_statistic = (mean - val)/std_dev
+                significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["rank"] == val)].assign(grubbs = grubbs_statistic))
     elif args.method == "scores":
         for term in ranked.iloc[:,1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
             tfidfs = ranked[ranked.iloc[:,1] == term]["tfidf_score"]  # use tf-idf scores
+            mean = np.mean(tfidfs)
+            std_dev = np.std(tfidfs)
             sig_values = grubbs.max_test_outliers(list(tfidfs), alpha=args.alpha)
-            significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["tfidf_score"].isin(sig_values))])
+            for val in sig_values:
+                grubbs_statistic = (val - mean)/std_dev
+                significant = significant.append(ranked[(ranked.iloc[:,1] == term) & (ranked["tfidf_score"] == val)].assign(grubbs = grubbs_statistic))
     
-    significant = significant.sort_values("rank", ascending=True)
+    if args.sort_by == "rank":
+        significant = significant.sort_values("rank", ascending=True)
+    else:
+        significant = significant.sort_values(args.sort_by, ascending=False)
     
     if not args.print_only:
         results_file = os.path.join("../results", args.run_name, "analysis", "significant_{}_{}_{}_{}_{}.csv".format(args.level, args.method, args.location, args.scatter, args.distance))
