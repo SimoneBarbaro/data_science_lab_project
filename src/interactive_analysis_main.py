70479ee6cc54fa8b901f515e7e73027dfd489006
nhutchins627 <56673379+nhutchins627@users.noreply.github.com>
2020-11-22 15:22:34 -0800

    find targets on all results files

--- src/interactive_analysis_main.py
+++ src/interactive_analysis_main.py
@@ -15,8 +15,22 @@ if __name__ == "__main__":
 						help="Sets the number of significant clusters to get")
 	parser.add_argument('--targets_per_cluster', type=int, default=5,
 						help="Sets the number of targets to find per cluster")
+	parser.add_argument('--all', action='store_true', default=False,
+						help="Whether or not to analyze all files at once")
 
 	args = parser.parse_args()
+	
+	if args.all:
+		list_subfolders_with_paths = [f.path for f in os.scandir('../results/') if f.is_dir()]
+		
+		for pth in list_subfolders_with_paths:
+			analyzer = InteractiveAnalyzer(pth)
+			df1, df2 = analyzer.get_important_data(args.level, args.num_to_get, args.targets_per_cluster)
+			df = pd.concat([pd.DataFrame(v) for k, v in df2.items()], axis = 1, keys = list(df2.keys()))
+		
+		targ_name = "targets_" + str(args.level) + ".csv"
+		targets_dir = os.path.join("../results/analysis", targ_name)
+		df.to_csv(os.path.join(analyzer.analysis_dir, targ_name), index = False)
 
 	results_dir = os.path.join("../results", args.run_name)
 	analyzer = InteractiveAnalyzer(results_dir)
