ca02e79d76d7c0a2a0b7f58f02a74db584718292
Simone <simone.barbaro2@studio.unibo.it>
2020-11-23 19:43:54 +0100

    fixed duplicate significants

--- src/experiment/statistical_analysis.py
+++ src/experiment/statistical_analysis.py
@@ -5,6 +5,7 @@ import pandas as pd
 from functools import reduce
 from outliers import smirnov_grubbs as grubbs  # pip install outlier_utils
 
+
 class StatisticalAnalyzer:
 
     def __init__(self, run_dir, method, alpha, sort_by, save=True, print_output=True):
@@ -33,30 +34,18 @@ class StatisticalAnalyzer:
         else:
             ranked = scores
 
-        term = "{}_term".format(level)
-
         significant = pd.DataFrame()
-        if self.method == "ranks":
-            for term in ranked.iloc[:, 1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
-                ranks = ranked[ranked.iloc[:, 1] == term]["rank"]  # use ranks
-                mean = np.mean(ranks)
-                std_dev = np.std(ranks)
-                sig_values = grubbs.min_test_outliers(list(ranks), alpha=self.alpha)
-                for val in sig_values:
-                    grubbs_statistic = (mean - val) / std_dev
-                    significant = significant.append(
-                        ranked[(ranked.iloc[:, 1] == term) & (ranked["rank"] == val)].assign(grubbs=grubbs_statistic))
-        elif self.method == "scores":
-            for term in ranked.iloc[:, 1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
-                tfidfs = ranked[ranked.iloc[:, 1] == term]["tfidf_score"]  # use tf-idf scores
-                mean = np.mean(tfidfs)
-                std_dev = np.std(tfidfs)
-                sig_values = grubbs.max_test_outliers(list(tfidfs), alpha=self.alpha)
-                for val in sig_values:
-                    grubbs_statistic = (val - mean) / std_dev
-                    significant = significant.append(
-                        ranked[(ranked.iloc[:, 1] == term) & (ranked["tfidf_score"] == val)].assign(
-                            grubbs=grubbs_statistic))
+        method_col = "rank" if self.method == "ranks" else "tfidf_score"
+
+        for term in ranked.iloc[:, 1].unique():  # scores.iloc[:,1] corresponds to soc_term/pt_term/etc.
+            values = ranked[ranked.iloc[:, 1] == term][method_col]
+            mean = np.mean(values)
+            std_dev = np.std(values)
+            sig_values = grubbs.min_test_outliers(list(values), alpha=self.alpha)
+            for val in set(sig_values):
+                grubbs_statistic = (mean - val) / std_dev
+                significant = significant.append(
+                    ranked[(ranked.iloc[:, 1] == term) & (ranked[method_col] == val)].assign(grubbs=grubbs_statistic))
 
         if self.sort_by == "rank":
             significant = significant.sort_values("rank", ascending=True)
