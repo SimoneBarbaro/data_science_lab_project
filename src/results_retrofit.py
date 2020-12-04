6e68a8e6ff1ef096ff46de8f720ee97bf08b563a
Simone <simone.barbaro2@studio.unibo.it>
2020-12-04 13:07:21 +0100

    fixed memory problems

--- src/results_retrofit.py
+++ src/results_retrofit.py
@@ -7,41 +7,34 @@ from data.read_data import load_full_matrix_with_names, get_twosides_meddra, fil
 
 subfolders_with_paths = [f.path for f in os.scandir("../results/") if f.is_dir()]
 meddra = get_twosides_meddra(False)
-data, names = load_full_matrix_with_names("spider")
-data_spider, names_spider = filter_twosides(data, names, meddra)
-data, names = load_full_matrix_with_names("tiger")
-data_tiger, names_tiger = filter_twosides(data, names, meddra)
+for dataset in ["spider", "tiger"]:
+    for to_filter in [False, True]:
+        data, names = load_full_matrix_with_names(dataset, to_filter)
+        data, names = filter_twosides(data, names, meddra)
 
-data, names = load_full_matrix_with_names("spider", True)
-data_spider_f, names_spider_f = filter_twosides(data, names, meddra)
-data, names = load_full_matrix_with_names("tiger", True)
-data_tiger_f, names_tiger_f = filter_twosides(data, names, meddra)
-for folder in subfolders_with_paths:
-    if not os.path.exists(os.path.join(folder, "results_info.json")):
-        if folder.count("tiger") > 0:
-            dataset = "tiger"
-            data = data_tiger
-        else:
-            dataset = "spider"
-            data = data_spider
-        try:
-            results_file = os.path.join(folder, "results.csv")
-            results = pd.read_csv(results_file)
-            try:
-                if data.values.shape[0] > len(results["cluster"]):
-                    if dataset == "tiger":
-                        data = data_tiger_f
-                    else:
-                        data = data_spider_f
-                score = silhouette_score(data.values, results["cluster"])
-                with open(os.path.join(folder, "results_info.json"), "w") as f:
-                    f.write(json.dumps({"dataset": dataset, "silhouette_score": score}))
-                    print(folder + " results info saved successfully")
-            except ValueError:
-                print(folder + " was built on too old version of dataset")
+        for folder in subfolders_with_paths:
+            if not os.path.exists(os.path.join(folder, "results_info.json")):
+                if folder.count("tiger") > 0 and dataset == "spider":
+                    continue
+                try:
+                    results_file = os.path.join(folder, "results.csv")
+                    results = pd.read_csv(results_file)
+                    try:
+                        if data.values.shape[0] != len(results["cluster"]):
+                            if not to_filter:
+                                continue
+                            else:
+                                raise ValueError
+                        score = silhouette_score(data.values, results["cluster"])
+                        with open(os.path.join(folder, "results_info.json"), "w") as f:
+                            f.write(json.dumps({"dataset": dataset, "match_datasets": to_filter,
+                                                "silhouette_score": score}))
+                            print(folder + " results info saved successfully")
+                    except ValueError:
+                        print(folder + " was built on too old version of dataset")
 
-                with open(os.path.join(folder, "results_info.json"), "w") as f:
-                    f.write(json.dumps({"dataset": dataset}))
-                
-        except FileNotFoundError:
-            print(folder + " results not found")
+                        with open(os.path.join(folder, "results_info.json"), "w") as f:
+                            f.write(json.dumps({"dataset": dataset, "match_datasets": to_filter}))
+
+                except FileNotFoundError:
+                    print(folder + " results not found")
